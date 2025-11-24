ada63c3f63c5ecf11311aa1c70b04ddc10470e16
elfege <elfege@elfege.com>
2025-11-24 01:08:24 -0500

    quick commit from alias

---
+++ config/ffmpeg_names_map.py
@@ -0,0 +1,6 @@
+ffmpeg_names_map = {
+    'frame_rate_full_screen': 'r',
+    'frame_rate_grid_mode': 'r',
+    'resolution_main': 'scale', # explicit: use 'scale' to mean "-vf scale=WxH"
+    'resolution_sub': 'scale'
+}

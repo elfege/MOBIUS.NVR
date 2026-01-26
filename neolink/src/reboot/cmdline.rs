ee43f83580838835b34a6155bd7dd170f593bc1d
elfege <elfege@elfege.com>
2026-01-26 13:34:06 -0500

    quick commit from alias

--- neolink/src/reboot/cmdline.rs
+++
@@ -1,8 +0,0 @@
-use clap::Parser;
-
-/// The reboot command will reboot the camera
-#[derive(Parser, Debug)]
-pub struct Opt {
-    /// The name of the camera to change the lights of. Must be a name in the config
-    pub camera: String,
-}
